c989fbc8607f075a4062c7c094576a9eee49e791
199Rich <bivens36@live.com>
2016-07-08 15:49:48 -0600

    Update submit.php

--- submit.php
+++ submit.php
@@ -2,7 +2,7 @@
 // if the url field is empty 
 if(isset($_POST['url']) && $_POST['url'] == ''){
      // then send the form to your email
-          mail( 'https://formspree.io/bivens36@live.com', 'Contact Form', print_r($_POST,true) ); 
+          mail( 'bivens36@live.com', 'Contact Form', print_r($_POST,true) ); 
 } 
 // otherwise, let the spammer think that they got their message through
 ?>
